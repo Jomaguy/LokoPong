c498ab49c341334d152dbf942672a30ef6c63ded
Jonathan Mahrt Guyou <jonathanmahrtguyou@gmail.com>
2025-04-22 15:31:28 -0400

    Initial Commit

---
+++ LokoPong/LokoPongApp.swift
@@ -0,0 +1,17 @@
+//
+//  LokoPongApp.swift
+//  LokoPong
+//
+//  Created by Jonathan Mahrt Guyou on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct LokoPongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LokoPongTests/LokoPongTests.swift
@@ -0,0 +1,17 @@
+//
+//  LokoPongTests.swift
+//  LokoPongTests
+//
+//  Created by Jonathan Mahrt Guyou on 4/22/25.
+//
+
+import Testing
+@testable import LokoPong
+
+struct LokoPongTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
